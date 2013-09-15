f3d3bac47afdb3313a8e7f462823ec3526fd92fe
Emery Denuccio <emery.denuccio@gmail.com>
2013-09-15 09:06:45 -0700

    add js source to examples

--- examples/star.js
+++ examples/star.js
@@ -1,19 +1,19 @@
 example('star', function(ctx) {
   star(ctx, 100, 100, 90, 5, 0.5);
-});
 
-function star(ctx, x, y, r, p, m)
-{
-    ctx.translate(x, y);
-    ctx.moveTo(0,0-r);
-    for (var i = 0; i < p; i++)
-    {
-        ctx.rotate(Math.PI / p);
-        ctx.lineTo(0, 0 - (r*m));
-        ctx.rotate(Math.PI / p);
-        ctx.lineTo(0, 0 - r);
-    }
-    ctx.fill();
-}
+  function star(ctx, x, y, r, p, m)
+  {
+      ctx.translate(x, y);
+      ctx.moveTo(0,0-r);
+      for (var i = 0; i < p; i++)
+      {
+          ctx.rotate(Math.PI / p);
+          ctx.lineTo(0, 0 - (r*m));
+          ctx.rotate(Math.PI / p);
+          ctx.lineTo(0, 0 - r);
+      }
+      ctx.fill();
+  }
+});
 
 
