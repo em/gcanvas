328476a1b75d1ce7d29aa8775011603f46bf9c58
Emery Denuccio <emery.denuccio@gmail.com>
2014-02-26 16:07:16 -0800

    Fix bug in arcToPoints. Was using mod pi instead of mod 2pi.

--- build/gcanvas.js
+++ build/gcanvas.js
@@ -9848,8 +9848,8 @@ module.exports = {\n\
    * Convert start+end angle arc to start/end points.\n\
    * */\n\
   arcToPoints: function(x, y, astart, aend, radius) {\n\
-    astart = astart % Math.PI;\n\
-    aend = aend % Math.PI;\n\
+    astart = astart % (Math.PI*2);\n\
+    aend = aend % (Math.PI*2);\n\
 \n\
     var a = new Point(), // start point\n\
         b = new Point(); // end point\n\
